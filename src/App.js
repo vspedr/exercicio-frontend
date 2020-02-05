e0790b1f305510e705d40beb35861ac6ae78a8fd
Victor Pedrazzi <vspedrazzi@gmail.com>
2020-02-05 18:10:13 -0300

    Remove unused import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { GlobalStyle } from './GlobalStyle';
-import { Glob } from 'glob';
 
 const App = () => (
   <>

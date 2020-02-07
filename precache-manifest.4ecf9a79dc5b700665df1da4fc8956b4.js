43ca3db26d59297d076b80a5ca92a56228c27ec7
Victor Pedrazzi <vspedrazzi@gmail.com>
2020-02-06 23:57:47 -0300

    Updates

---
+++ precache-manifest.4ecf9a79dc5b700665df1da4fc8956b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb4af8e5416e4cbb6a9d8e0a5011bd76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acb818ae450bea78cf2d",
+    "url": "/static/js/2.81da50c1.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.81da50c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa160c5d631f3bd83ef8",
+    "url": "/static/js/main.864af176.chunk.js"
+  },
+  {
+    "revision": "31f3b4fc786053d3ad0a",
+    "url": "/static/js/runtime-main.b24dcae1.js"
+  }
+]);

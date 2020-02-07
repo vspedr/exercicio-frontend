6af9b03f20d4bd52dce64e8ad185d22202c5c7a3
Victor Pedrazzi <vspedrazzi@gmail.com>
2020-02-06 23:59:16 -0300

    Updates

---
+++ precache-manifest.471d06b577038ac5454ebf20929bbe18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff8ca7bc1caa6d9d4401aa4744d111d6",
+    "url": "/exercicio-frontend/index.html"
+  },
+  {
+    "revision": "a8267064c776e5907f4b",
+    "url": "/exercicio-frontend/static/js/2.ba2be243.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/exercicio-frontend/static/js/2.ba2be243.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "197c913383fa3abd16cc",
+    "url": "/exercicio-frontend/static/js/main.676bd650.chunk.js"
+  },
+  {
+    "revision": "9ac73af2023443c3fb59",
+    "url": "/exercicio-frontend/static/js/runtime-main.0f05befd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4ecf9a79dc5b700665df1da4fc8956b4.js"
+  "/exercicio-frontend/precache-manifest.471d06b577038ac5454ebf20929bbe18.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/exercicio-frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
